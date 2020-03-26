a7a114472d8241a3509fe15ce292b30eab13102a
HUSEYNIYON <your>
2020-03-26 19:49:26 +0300

    created console.net core project

--- Program.cs
+++ Program.cs
@@ -6,64 +6,64 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-            // //Exercise_2
-            // Console.WriteLine("Exercise_2");
-            // int A=Convert.ToInt32(Console.ReadLine());
-            // int B=Convert.ToInt32(Console.ReadLine());
-            // Console.WriteLine("A="+A);
-            // Console.WriteLine("B="+B);
-            // if(A!=B)
-            // {
-            //     if(A>B)
-            //     {
-            //         Console.WriteLine("A="+A);
-            //         Console.WriteLine("B="+(B=A));
-            //     }
-            //     else
-            //     {
-            //         Console.WriteLine("A="+(A=B));
-            //         Console.WriteLine("B="+B);
-            //     }                
-            // }
-            // else if(A==B)
-            //      {
-            //     Console.WriteLine("A="+(A=0));
-            //     Console.WriteLine("B="+(B=0));
-            //      }
-            // Console.ReadLine();
+            //Exercise_2
+            Console.WriteLine("Exercise_2");
+            int A=Convert.ToInt32(Console.ReadLine());
+            int B=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A="+A);
+            Console.WriteLine("B="+B);
+            if(A!=B)
+            {
+                if(A>B)
+                {
+                    Console.WriteLine("A="+A);
+                    Console.WriteLine("B="+(B=A));
+                }
+                else
+                {
+                    Console.WriteLine("A="+(A=B));
+                    Console.WriteLine("B="+B);
+                }                
+            }
+            else if(A==B)
+                 {
+                Console.WriteLine("A="+(A=0));
+                Console.WriteLine("B="+(B=0));
+                 }
+            Console.ReadLine();
 
-            // //Exercise_2
-            // Console.WriteLine("Exercise_3");
-            // float a=15f;
-            // float b=0f;
-            // Console.Write("Enter a arithmetic sign: ");
-            // string sign=Console.ReadLine();
-            // switch(sign)
-            // {
-            //     case "+":
-            //         Console.WriteLine("Sum="+(a+b));
-            //         break;
-            //     case "-":
-            //         Console.WriteLine("Min="+(a-b));
-            //         break;
-            //     case "*":
-            //         Console.WriteLine("Mul="+(a*b));
-            //         break;
-            //     case "/":
-            //         if(b==0)
-            //         {
-            //             Console.WriteLine("Out of range!");
-            //             break;
-            //         }
-            //         else
-            //             Console.WriteLine("Div="+(a/b));
-            //         break;
-            //     default:
-            //         Console.WriteLine("Incorrect sign!");
-            //         break;
+            //Exercise_3
+            Console.WriteLine("Exercise_3");
+            float a=15f;
+            float b=0f;
+            Console.Write("Enter a arithmetic sign: ");
+            string sign=Console.ReadLine();
+            switch(sign)
+            {
+                case "+":
+                    Console.WriteLine("Sum="+(a+b));
+                    break;
+                case "-":
+                    Console.WriteLine("Min="+(a-b));
+                    break;
+                case "*":
+                    Console.WriteLine("Mul="+(a*b));
+                    break;
+                case "/":
+                    if(b==0)
+                    {
+                        Console.WriteLine("Out of range!");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Div="+(a/b));
+                    break;
+                default:
+                    Console.WriteLine("Incorrect sign!");
+                    break;
                     
-            // }
-            // Console.ReadLine();
+            }
+            Console.ReadLine();
 
             //Exercise_4
             Console.WriteLine("Exercise_4");
